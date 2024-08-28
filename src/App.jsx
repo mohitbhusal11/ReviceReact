eac631a73c103bc832718e94527e353378793e87
mohit <mohitbhusal11@gmail.com>
2024-08-28 23:24:21 +0530

    Git init and some revised in react like folder structure and creating react app using vite.

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className='main'>
+      <h1 className='innerH1'>Hello from h1 inside the main tag</h1>
+    </div>
+  )
+}
+
+export default App
